9823a2b6d6f095c55a274a4db7cf44ccaf1f3b72
Dao Bui <dao.hunter@gmail.com>
2014-06-06 01:43:03 +0700

    Initial Controller

---
+++ application/controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php
+class Controller extends CI_Controller {
+    public $pageTitle;
+    public $metaTag;
+    public $contentTag;
+    public $description;
+
+}
--- raovat.sql
+++ raovat.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 05, 2014 at 11:20 AM
--- Server version: 5.5.35-0ubuntu0.13.10.2
--- PHP Version: 5.5.3-1ubuntu2.1
+-- Generation Time: Jun 06, 2014 at 01:35 AM
+-- Server version: 5.5.32-0ubuntu7
+-- PHP Version: 5.5.3-1ubuntu2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,13 +29,26 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `root` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `page_title` varchar(255) NOT NULL,
   `content_tag` varchar(255) NOT NULL,
+  `category_type` tinyint(3) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-UNIQUE KEY `slug` (`slug`)
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE IF NOT EXISTS `city` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -50,7 +63,14 @@ CREATE TABLE IF NOT EXISTS `config` (
   `value` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`id`, `name`, `value`) VALUES
+(1, 'test3', 'test');
 
 -- --------------------------------------------------------
 
@@ -73,11 +93,12 @@ CREATE TABLE IF NOT EXISTS `friend` (
 
 CREATE TABLE IF NOT EXISTS `message` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `author` int(11) NOT NULL,
-  `receiver` int(11) NOT NULL,
+  `sender` int(11) NOT NULL,
+  `recipient` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_read` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -110,8 +131,10 @@ CREATE TABLE IF NOT EXISTS `post` (
   `content` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `author` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-	UNIQUE KEY `slug` (`slug`)
+  UNIQUE KEY `slug` (`slug`),
+  UNIQUE KEY `city_id` (`city_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -193,6 +216,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `address` varchar(255) NOT NULL,
   `avatar` varchar(255) NOT NULL,
   `role` tinyint(4) NOT NULL DEFAULT '0',
+  `is_activate` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
