a32dd865c3a30a440a3f5400d496aae6746c24c2
daobv <dao.hunter@gmail.com>
2014-06-10 18:21:10 +0700

    User avatar

--- application/backend/controllers/User_controller.php
+++ application/backend/controllers/User_controller.php
@@ -22,13 +22,18 @@ class User_controller extends Controller{
         $crud->set_table('user');
         $crud->columns('username','gender','first_name','last_name','dob','email','phone');
         $crud->display_as('username','Tài Khoản')
+             ->display_as('password','Mật Khẩu')
             ->display_as('gender','Giới Tính')
             ->display_as('first_name','Tên')
             ->display_as('last_name','Họ')
             ->display_as('dob','Ngày Sinh')
             ->display_as('email','Email')
-            ->display_as('phone','Điện Thoại');
+            ->display_as('phone','Điện Thoại')
+            ->display_as('avatar','Ảnh đại diện')
+            ->field_type('password','password')
+            ->set_field_upload('avatar','../assets/uploads/');
         $crud->set_subject('user');
+
         $output = $crud->render();
         $this->_example_output($output);
     }
