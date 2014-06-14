5f9ca122f051abd9667f0d09b8809853572e7bea
daobv <daobv@daobv-ThinkPad-T410.(none)>
2014-06-15 01:15:51 +0700

    user layout

---
+++ application/views/user/info_block.php
@@ -0,0 +1,17 @@
+<div id="left-sidebar">
+    <div class="user-menu-box sidebar-box">
+        <h3 class="sidebar-title"><a href="">Thông tin chung</a></h3>
+        <h5>Thông tin thành viên</h5>
+        <ul>
+            <li><a href="">Tin nhắn</a></li>
+            <li><a href="">Đổi thông tin cá nhân</a></li>
+            <li><a href="">Đổi mật khẩu</a></li>
+            <li><a href="">Thoát ra</a></li>
+        </ul>
+        <h5>Thông tin thành viên</h5>
+        <ul>
+            <li><a href="">Quản lý đăng tin</a></li>
+            <li><a href="">Tin đã lưu</a></li>
+        </ul>
+    </div>
+</div>
